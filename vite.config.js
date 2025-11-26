163bb344db11199ab68e9f9c7283b951e5f7763c
Kevin <kevinxia2024@gmail.com>
2025-11-25 18:20:03 -0800

    added courses

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,27 @@ export default defineConfig({
             },
         },
     },
+    server: {
+        // Middleware to handle clean URLs
+        middlewareMode: false,
+    },
+    appType: 'mpa',
+    plugins: [
+        {
+            name: 'rewrite-middleware',
+            configureServer(server) {
+                server.middlewares.use((req, res, next) => {
+                    // Rewrite clean URLs to .html files
+                    if (req.url === '/learnings') {
+                        req.url = '/learnings.html';
+                    } else if (req.url === '/blog') {
+                        req.url = '/blog.html';
+                    } else if (req.url === '/books') {
+                        req.url = '/books.html';
+                    }
+                    next();
+                });
+            },
+        },
+    ],
 });
